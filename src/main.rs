90fcb38d6fc795f08339afe4f8b175c116af96b1
Bas <b.vanschoonhoven@gmail.com>
2021-11-21 09:47:41 +0100

    Sidebars for gui layout

--- src/main.rs
+++ src/main.rs
@@ -50,13 +50,13 @@ fn setup(
     mut commands: Commands,
     mut textures: ResMut<Assets<Texture>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
-    level: Res<SandBox>,
+    sandbox: Res<SandBox>,
 ) {
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
 
     // Create texture for displaying
     let texture = Texture::new_fill(
-        Extent3d::new(level.width() as u32, level.height() as u32, 1),
+        Extent3d::new(sandbox.width() as u32, sandbox.height() as u32, 1),
         TextureDimension::D2,
         &[0, 0, 0, 255],
         TextureFormat::Rgba8Unorm,
@@ -80,40 +80,44 @@ fn gui_system(
     mut simulation: ResMut<Simulation>,
     mut level: ResMut<SandBox>,
 ) {
-    egui::Window::new("Tools").show(egui_context.ctx(), |ui| {
-        ui.label("Element:");
-        ui.radio_value(&mut toolbox.element, Element::Air, "Air");
-        ui.radio_value(&mut toolbox.element, Element::Sand, "Sand");
-        ui.radio_value(&mut toolbox.element, Element::Wood, "Wood");
-        ui.radio_value(&mut toolbox.element, Element::Rock, "Rock");
-        ui.radio_value(&mut toolbox.element, Element::Water, "Water");
-        ui.radio_value(&mut toolbox.element, Element::Acid, "Acid");
-        ui.radio_value(&mut toolbox.element, Element::Oil, "Oil");
-        ui.radio_value(&mut toolbox.element, Element::Lava, "Lava");
-        ui.radio_value(&mut toolbox.element, Element::Fire, "Fire");
-        ui.radio_value(&mut toolbox.element, Element::WaterSource, "Water source");
-        ui.radio_value(&mut toolbox.element, Element::AcidSource, "Acid Source");
-        ui.radio_value(&mut toolbox.element, Element::OilSource, "Oil source");
-        ui.radio_value(&mut toolbox.element, Element::LavaSource, "Lava source");
-        ui.radio_value(&mut toolbox.element, Element::FireSource, "Fire source");
-        ui.radio_value(&mut toolbox.element, Element::Drain, "Liquid drain");
-        ui.label("Tool size:");
-        ui.add(egui::Slider::new(&mut toolbox.tool_size, 1..=64));
-        ui.label("Tool type:");
-        ui.radio_value(&mut toolbox.tool, Tool::FillCircle, "Fill Circle");
-        ui.radio_value(&mut toolbox.tool, Tool::FillSquare, "Fill Square");
-        ui.radio_value(&mut toolbox.tool, Tool::SprayCircle, "Spray Circle");
-    });
+    egui::SidePanel::left("left_tools")
+        .min_width(180.0)
+        .show(egui_context.ctx(), |ui| {
+            ui.label("Tool element:");
+            ui.radio_value(&mut toolbox.element, Element::Air, "Air");
+            ui.radio_value(&mut toolbox.element, Element::Sand, "Sand");
+            ui.radio_value(&mut toolbox.element, Element::Wood, "Wood");
+            ui.radio_value(&mut toolbox.element, Element::Rock, "Rock");
+            ui.radio_value(&mut toolbox.element, Element::Water, "Water");
+            ui.radio_value(&mut toolbox.element, Element::Acid, "Acid");
+            ui.radio_value(&mut toolbox.element, Element::Oil, "Oil");
+            ui.radio_value(&mut toolbox.element, Element::Lava, "Lava");
+            ui.radio_value(&mut toolbox.element, Element::Fire, "Fire");
+            ui.radio_value(&mut toolbox.element, Element::WaterSource, "Water source");
+            ui.radio_value(&mut toolbox.element, Element::AcidSource, "Acid Source");
+            ui.radio_value(&mut toolbox.element, Element::OilSource, "Oil source");
+            ui.radio_value(&mut toolbox.element, Element::LavaSource, "Lava source");
+            ui.radio_value(&mut toolbox.element, Element::FireSource, "Fire source");
+            ui.radio_value(&mut toolbox.element, Element::Drain, "Liquid drain");
+            ui.separator();
+            ui.label("Tool size:");
+            ui.add(egui::Slider::new(&mut toolbox.tool_size, 1..=64));
+            ui.separator();
+            ui.label("Tool shape:");
+            ui.radio_value(&mut toolbox.tool, Tool::FillCircle, "Fill Circle");
+            ui.radio_value(&mut toolbox.tool, Tool::FillSquare, "Fill Square");
+            ui.radio_value(&mut toolbox.tool, Tool::SprayCircle, "Spray Circle");
+        });
 
-    egui::Window::new("Simulation").show(egui_context.ctx(), |ui| {
+    egui::SidePanel::right("right_tools").show(egui_context.ctx(), |ui| {
+        ui.label("Simulation:");
         ui.checkbox(&mut simulation.running, "Running");
         if ui.button("Step").clicked() {
             simulation.step = true;
         }
-        ui.label(format!("Frametime: {} ms", simulation.frame_time_ms));
-    });
-
-    egui::Window::new("Level").show(egui_context.ctx(), |ui| {
+        ui.label(format!("Step time: {} ms", simulation.frame_time_ms));
+        ui.separator();
+        ui.label("Sandbox:");
         if ui.button("Clear").clicked() {
             level.clear();
         }
